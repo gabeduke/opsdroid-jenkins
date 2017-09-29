ed694abc5cfa449d3201205dbfdfa0d4bc358e99
gabeduke <gabeduke@gmail.com>
2017-09-29 13:45:45 -0400

    add jenkins skill

---
+++ __init__.py
@@ -0,0 +1,29 @@
+import random
+import jenkins
+import logging
+
+from opsdroid.matchers import match_regex
+
+_LOGGER = logging.getLogger(__name__)
+
+class BuildServer:
+    def __init__(self, config, job):
+        self.job = job
+        try:
+            username = config['credentials']['username']
+            password = config['credentials']['password']
+        except FileNotFoundError:
+            _LOGGER.error('jenkins_credentials.yaml not found. Please copy from template in '
+                          'config/jenkins_credentials.yaml.example')
+
+        server = jenkins.Jenkins('http://jenkins:8080', username=username, password=password)
+        server.build_job(self.job)
+
+
+@match_regex(r'build', case_sensitive=False)
+async def build_job(opsdroid, config, message):
+    try:
+        await message.respond(random.choice(config['language']['build-confirm']))
+        BuildServer(config, 'builder')
+    except KeyError:
+        await message.respond('Configuration not found')
