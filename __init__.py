cec2a7de37ce822b3edafcf51a72f8a458c19bfa
gabeduke <gabeduke@gmail.com>
2017-10-02 01:01:50 -0400

    parse for passed job

--- __init__.py
+++ __init__.py
@@ -1,4 +1,3 @@
-import random
 import jenkins
 import logging
 
@@ -6,9 +5,12 @@ from opsdroid.matchers import match_regex
 
 _LOGGER = logging.getLogger(__name__)
 
-class BuildServer:
-    def __init__(self, config, job):
-        self.job = job
+
+class JenkinsBot:
+    def __init__(self, config, **kwargs):
+
+        self.job = kwargs.get('job_name')
+
         try:
             username = config['credentials']['username']
             password = config['credentials']['password']
@@ -17,14 +19,33 @@ class BuildServer:
             _LOGGER.error('jenkins_credentials.yaml not found. Please copy from template in '
                           'config/jenkins_credentials.yaml.example')
 
-        server = jenkins.Jenkins(url, username=username, password=password)
-        server.build_job(self.job)
+        self.server = jenkins.Jenkins(url, username=username, password=password)
+
+    def build_job(self):
+
+        # get list of jobs
+        jobs = self.server.get_jobs(5)
+        matches = []
+
+        # scrape list of jobs for matches. append to list
+        for i in jobs:
+            job_path = i['fullname']
+            # TODO if match -> matches.append(job_path)
+
+        # TODO if matches only has one match:
+        if():
+            try:
+                self.server.build_job(matches[0])
+                return matches[0]
+            except jenkins.NotFoundException:
+                _LOGGER.info('job name {} found but path {} is incorrect'.format(self.job, job_path))
+        # TODO else matches > 1:
+        else:
+            return matches
+            # TODO return the list for user selelection
 
 
-@match_regex(r'build', case_sensitive=False)
+@match_regex(r"build (?P<job>\w+)", case_sensitive=False)
 async def build_job(opsdroid, config, message):
-    try:
-        await message.respond('Job building..')
-        BuildServer(config, 'builder')
-    except KeyError:
-        await message.respond('Configuration not found')
+    res = JenkinsBot.build_job(message.regex.group('job'))
+    await message.respond('Job building..')
